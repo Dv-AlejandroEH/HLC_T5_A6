6c03d53f3270c8489527f00fbd5848d59186a43c
Dv-AlejandroEH <aespher121@gmail.com>
2025-02-20 08:18:37 +0100

    Finished

---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+def ordenar(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+    return lista
+
+print(ordenar([64, 34, 25, 12, 22, 11, 90]))
